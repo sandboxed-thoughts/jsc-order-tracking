436c0629194d584baab21c335d8358d0455b38d8
Nate Schram <nate@sandboxedthoughts.com>
2022-02-27 18:00:39 -0500

    fix static_root

--- tracker/tracker/settings.py
+++ tracker/tracker/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = str(BASE_DIR.joinpath('public/static')),
+STATIC_ROOT = BASE_DIR.joinpath('public/static'),
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
