ae71bd8175b70f41a481fe7570c7c332c1664a2b
Nate <nate@sandboxedthoughts.com>
2022-03-14 07:38:24 -0400

    form updates
    updating custom form for concrete orders

--- project/apps/concrete/forms/order_forms.py
+++ project/apps/concrete/forms/order_forms.py
@@ -1,26 +1,26 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.core.validators import RegexValidator
-
 from apps.clients.helpers import LotChoices
 from apps.core.helpers import get_choices
 from apps.suppliers.models import Supplier
-
+from apps.clients.models import Lot
 from ..helpers import MixChoices
 
 
 class BaseConcreteOrderForm(forms.ModelForm):
     """Custom form to handle creation of wall orders"""
 
-    supplier_choices = get_choices(Supplier.objects.filter(is_active=True), "name")
+    supplier_choices = get_choices(list(Supplier.objects.filter(is_active=True)), "name")
 
-    user = get_user_model()
+    User = get_user_model()
     po = forms.CharField(
         label="purchase order".title(), max_length=50, required=True, validators=[RegexValidator("[\\S\\w]")]
     )
-    lots = forms.MultipleChoiceField(label="lots".title(), choices=LotChoices.choices)
-    supplier = forms.ChoiceField(label="supplier".title(), choices=supplier_choices)
-    dispatcher = forms.ChoiceField(label="dispatcher".title(), choices=user.objects.filter(groups__name="dispatchers"))
+    # https://simpleisbetterthancomplex.com/tutorial/2019/01/02/how-to-implement-grouped-model-choice-field.html
+    lots = forms.ModelMultipleChoiceField(label="lots".title(), queryset=Lot.objects.all())
+    supplier = forms.ModelChoiceField(label="supplier".title(), queryset=Supplier.objects.filter(is_active=True))
+    dispatcher = forms.ModelChoiceField(label="dispatcher".title(), queryset=User.objects.filter(groups__name="Dispatchers"))
     etotal = forms.IntegerField(label="estimated total".title())
     atotal = forms.IntegerField(label="actual total".title(), required=False)
     qordered = forms.IntegerField(
--- project/apps/core/helpers/__init__.py
+++ project/apps/core/helpers/__init__.py
@@ -1,1 +1,1 @@
-from .form_helpers import get_choice_list, get_choices
+from .form_helpers import get_choice_list, get_choices, GroupedModelChoiceField, GroupedModelChoiceIterator
